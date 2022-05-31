bdf1fec5a9f074c97c4c4a5d1beb4512bec106d4
joshua530 <joshuaomari5@gmail.com>
2022-05-31 16:14:31 +0300

    update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,6 +6,13 @@
  * @list: the list to check
  *
  * Description: uses Floyd’s Cycle-Finding Algorithm
+ * -------------------------------
+ * A hash table can also be used:
+ * Traverse the given linked list and put the address of each node in a Hash table.
+ * If you reach a node whose next pointer is NULL, then given linked list doesn't
+ * contain s cycle.
+ * If address of any node already exist in Hash table, it means we are visiting this node
+ * again and linked list contains a cycle.
  *
  * Return: 0 if no cycle; 1 if  cycle exists
  */
