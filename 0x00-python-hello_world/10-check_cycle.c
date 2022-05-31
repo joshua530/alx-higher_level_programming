94643cabbc288a829605ed9d4c0c60041a4cca7a
joshua530 <joshuaomari5@gmail.com>
2022-05-31 16:10:10 +0300

    update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,25 +5,23 @@
  *
  * @list: the list to check
  *
+ * Description: uses Floyd’s Cycle-Finding Algorithm
+ *
  * Return: 0 if no cycle; 1 if  cycle exists
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *tmp;
-	listint_t *head = list;
+	listint_t *head = tmp = list;
 
-	while (head != NULL)
+	while (head && tmp && tmp->next)
 	{
-		tmp = head->next;
-		while (tmp != NULL)
-		{
-			if (head == tmp) /* back to where we started */
-			{
-				return (1);
-			}
-			tmp = tmp->next;
-		}
 		head = head->next;
+		tmp = tmp->next->next;
+		if (head == tmp) /* back to where we started */
+		{
+			return (1);
+		}
 	}
 
 	return (0);
