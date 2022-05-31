c9098469429b6cdc13b1791a8b438eef43bb6adf
joshua530 <joshuaomari5@gmail.com>
2022-05-31 12:45:16 +0300

    add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def uppercase(str):
+	to_print = ""
+	for i in range(len(str)):
+		ascii = ord(str[i])
+		if ascii >= 65 and ascii <= 90: # uppercase already
+			to_print += str[i]
+		elif ascii >= 97 and ascii <= 122: # lowercase
+			to_print += chr(ord(str[i]) - 32)
+		else: # other characters
+			to_print += str[i]
+	print(f"{to_print:s}")
