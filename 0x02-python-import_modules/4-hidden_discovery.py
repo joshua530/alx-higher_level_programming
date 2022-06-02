17f8d998366ac50e23ca92e29fc99b79caa0e96c
joshua530 <joshuaomari5@gmail.com>
2022-06-02 13:01:37 +0300

    update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     names = dir(hidden_4)
     names.sort()
     for name in names:
-        if name[0] != "_":
+        if not name.startswith("__"):
             print(name)
