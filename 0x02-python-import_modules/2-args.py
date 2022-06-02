0e4e232deac106bef88cc6435346f71cefa9f590
joshua530 <joshuaomari5@gmail.com>
2022-06-02 12:43:55 +0300

    add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    size = len(argv)
+    end_clause = "arguments:"
+    if size == 2:
+        end_clause = "argument."
+    elif size == 1:
+        end_clause = "arguments."
+
+    print("{} {}".format(size - 1, end_clause))
+
+    for i in range(1, size):
+        print("{}: {}".format(i, argv[i]))
