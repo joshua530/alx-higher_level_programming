1cba59dc4cbb0d239bc9c8e463dbbfee60303563
joshua530 <joshuaomari5@gmail.com>
2022-06-05 15:33:15 +0300

    add documentation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,14 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+    """prints out a matrix made up of integers
+
+    Args:
+        matrix: the matrix to be printed out
+
+    Returns:
+        None
+    """
     for arr in matrix:
         for i in range(len(arr)):
             if i == len(arr) - 1:
