f7695cba3460ec45d70f2b26c435ff9de3ea1f26
joshua530 <joshuaomari5@gmail.com>
2022-06-08 16:15:12 +0300

    weight_average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    """calculates weighted average
+    
+    Args:
+        my_list: the list whose weighted average we are finding
+        
+    Returns:
+        weighted average or 0 if calculation failed
+    """
+    numerator = 0
+    denominator = 0
+    for num_set in my_list:
+        numerator += num_set[0] * num_set[1]
+        denominator += num_set[1]
+    if denominator == 0:
+        return 0
+    return numerator / denominator
