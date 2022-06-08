14ec871363bc986b67dbe86a4e3e8fe3b20a3a52
joshua530 <joshuaomari5@gmail.com>
2022-06-08 16:03:31 +0300

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,7 +13,9 @@ def roman_to_int(roman_string):
     """
     if roman_string is None or roman_string.__class__.__name__ != "str":
         return 0
-    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_numerals = {
+        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+    }
     sum = 0
     for char in roman_string:
         if char not in roman_numerals:
