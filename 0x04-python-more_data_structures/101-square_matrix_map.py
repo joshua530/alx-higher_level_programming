c16568a4f270a14d2ca8c708194072cbf449dee3
joshua530 <joshuaomari5@gmail.com>
2022-06-08 16:37:35 +0300

    fulfill requirements

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,20 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    """squares a matrix's values using the map method
-
-    the returned matrix is different from the one passed
-    into the function
-
-    Args:
-        matrix: the matrix to square
-
-    Returns:
-        the square matrix"""
-    squared = []
-
-    for num_list in matrix:
-        squared.append(list(map(lambda x: x*x, num_list)))
-
-    return squared
+    return list(map(lambda num_list: list(map(lambda num: num * num, num_list)), matrix))
