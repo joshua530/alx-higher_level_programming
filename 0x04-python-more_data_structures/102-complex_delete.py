fbf480242ecd2259f2fcc4bf39c68921166ecebf
joshua530 <joshuaomari5@gmail.com>
2022-06-08 16:49:02 +0300

    remove trailing whitespace

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,11 +2,11 @@
 
 def complex_delete(a_dictionary, value):
     """deletes key which maps to a certain value from dictionary
-    
+
     Args:
         a_dictionary: the dictionary to modify
         value: value whose key[s] are to be deleted
-        
+
     Returns:
         dictionary whose keys that map to value have been deleted
     """
