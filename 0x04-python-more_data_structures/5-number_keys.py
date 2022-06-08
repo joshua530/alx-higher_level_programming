601301647cac6846839c113f6d6dfd0d012ddd65
joshua530 <joshuaomari5@gmail.com>
2022-06-08 13:59:04 +0300

    number_keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary: dict):
+    """finds the number of keys in a dictionary
+
+    Args:
+        a_dictionary: the dictionary whose key count we want
+
+    Return:
+        number of keys in dictionary
+    """
+    return len(a_dictionary.keys())
