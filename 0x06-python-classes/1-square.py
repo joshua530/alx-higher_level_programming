93037404fd1cff44c847b2dc6537f5d8f1f468a3
joshua530 <joshuaomari5@gmail.com>
2022-06-21 11:55:55 +0300

    instantiate square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """A class that represents squares"""
+
+    def __init__(self, size):
+        """
+        Instantiates a square
+
+        Args:
+            size: the size of the square sides
+        """
+        self.__size = size
