75e7388f8e2a810ae2717c5e21030e600e77a1c6
joshua530 <joshuaomari5@gmail.com>
2022-06-21 10:02:53 +0300

    catch TypeError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+from typing import Type
+
+
 def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         pass
     finally:
         print("Inside result = {}".format(result))
