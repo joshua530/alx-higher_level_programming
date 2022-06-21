0be798514e45098a4d92e8c0f031517120fdc20e
joshua530 <joshuaomari5@gmail.com>
2022-06-21 12:02:02 +0300

    add square method to square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """A class that represents squares"""
+
+    def __init__(self, size=0):
+        """
+        Instantiates a square
+
+        Args:
+            size: the size of the square sides
+        """
+        if size.__class__.__name__ != "int":
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculates square area"""
+        return self.__size * self.__size
