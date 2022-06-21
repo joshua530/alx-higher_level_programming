9bc3312a1192907e44d5fc5f2103053c0ed3c643
joshua530 <joshuaomari5@gmail.com>
2022-06-21 12:23:06 +0300

    print empty line for 0 sized square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -49,6 +49,8 @@ class Square:
 
     def my_print(self):
         """Prints the square"""
+        if self.__size == 0:
+            print("")
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
