1dc7eeee528c93c1af0f704319a8b2fe03747481
joshua530 <joshuaomari5@gmail.com>
2022-06-27 08:17:07 +0300

    implement text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Contains a text indentation function
+"""
+
+
+def text_indentation(text):
+    """
+    adds newlines to text
+
+    It prints two newlines after the characters . ? :
+
+    Args:
+        text: the text to print out
+
+    Raises:
+        TypeError: if text is not a string
+    """
+    if type(text) not in [str]:
+        raise TypeError("text must be a string")
+    for char in text:
+        if char in ["?",":","."]:
+            print(char, end="\n\n")
+        else:
+            print(char, end="")
