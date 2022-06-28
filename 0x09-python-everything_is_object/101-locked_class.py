bd129f886acbd2b1c85c6cf4e7a3a92160606aad
joshua530 <joshuaomari5@gmail.com>
2022-06-28 09:18:34 +0300

    add doctests

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,6 +9,13 @@ class LockedClass:
     Restricts dynamic creation of instance attributes
 
     Only first_name is allowed
+
+    >>> x = LockedClass()
+    >>> x.first_name = 'foo'
+    >>> x.last_name = 'bar'
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'LockedClass' object has no attribute 'last_name'
     """
     
     __slots__ = "first_name"
