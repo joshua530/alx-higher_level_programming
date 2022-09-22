164a485ae8ead5e63244aeb207a45ec360df8d75
joshua530 <joshuaomari5@gmail.com>
2022-09-22 14:30:16 +0300

    reduce line number

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends request to a given url and only displays response if status code
-# is 200
+# sends request to a given url and only displays response if status code is 200
 curl -sL "$1"
