82a1d092fc7870008fda1104acaff833d62c34f9
joshua530 <joshuaomari5@gmail.com>
2022-09-22 14:52:48 +0300

    use silent mode

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends post request to given url and displays response
-curl -sX POST -d'email=test@gmail.com&subject=I will always be here for PLD' "$1"
+curl -sX POST -d'email=test@gmail.com' -d'subject=I will always be here for PLD' "$1"
