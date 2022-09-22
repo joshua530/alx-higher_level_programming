c404136319b958ffd1aa3acd0af3e07dd65f3515
joshua530 <joshuaomari5@gmail.com>
2022-09-22 15:47:10 +0300

    make pycodestyle compliant

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,8 @@ Contains function that finds the peak in a given list of numbers
 def find_peak(list_of_integers):
     """Finds the peak in a given list of integers
 
-    Peak: a number n in the list where n is greater than each of it's neighbours
+    Peak: a number n in the list where n is greater than each of
+    it's neighbours
     """
     if len(list_of_integers) == 0:
         return None
@@ -17,19 +18,19 @@ def find_peak(list_of_integers):
     mid = len(list_of_integers) // 2
     left = 0
     right = len(list_of_integers) - 1
-    l_ints = list_of_integers
+    l_i = list_of_integers
 
-    if l_ints[0] > l_ints[1]:
-        return l_ints[0]
-    if l_ints[right] > l_ints[right - 1]:
-        return l_ints[right]
+    if l_i[0] > l_i[1]:
+        return l_i[0]
+    if l_i[right] > l_i[right - 1]:
+        return l_i[right]
 
     while left <= mid:
         left += 1
         right -= 1
 
-        if l_ints[left] >= l_ints[left - 1] and l_ints[left] >= l_ints[left + 1]:
-            return l_ints[left]
-        if l_ints[right] >= l_ints[right + 1] and l_ints[right] >= l_ints[right - 1]:
-            return l_ints[right]
+        if l_i[left] >= l_i[left - 1] and l_i[left] >= l_i[left + 1]:
+            return l_i[left]
+        if l_i[right] >= l_i[right + 1] and l_i[right] >= l_i[right - 1]:
+            return l_i[right]
     return None
