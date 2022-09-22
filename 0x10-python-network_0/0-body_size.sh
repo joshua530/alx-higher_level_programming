d128416caa5d07d4035debb0f5bf1da349138943
joshua530 <joshuaomari5@gmail.com>
2022-09-22 14:34:40 +0300

    update make grep string case sensitive

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a url, sends a request to the url and displays the size of the response body
-curl -sI "$1" | grep -i 'content-length:' | cut -d' ' -f2
+curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
