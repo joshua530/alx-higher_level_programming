318e508925cc992746c83ee07defe88320014ca3
joshua530 <joshuaomari5@gmail.com>
2022-09-23 14:10:32 +0300

    correct parameter placement in url

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     repo = sys.argv[1]
     owner = sys.argv[2]
     r = requests.get(
-        'https://api.github.com/repos/{}/{}/commits?page=1&per_page=10'.format(repo, owner))
+        'https://api.github.com/repos/{}/{}/commits?page=1&per_page=10'
+        .format(owner, repo))
     commits = r.json()
     for commit in commits:
         author = commit.get('commit').get('author').get('name')
